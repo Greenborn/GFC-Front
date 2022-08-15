0d4acdfbfba545a8b03be45c294ae9832d2f85ac
Luciano Vega <lucho.2012.tandil@gmail.com>
2022-08-15 01:56:34 -0300

    Se termina de definir el ranking

--- src/app/concursos/ranking/ranking.page.ts
+++ src/app/concursos/ranking/ranking.page.ts
@@ -23,8 +23,16 @@ export class RankingPage implements OnInit {
     
   }
 
+  obtener_filas( categoria ){
+    if (categoria.pestania_seccion == -1){
+      return categoria.profiles;
+    }
+
+    return categoria.ranks_seccion[ categoria.pestania_seccion ].profiles
+  }
+
   procesar_ranking( r ){
-    let ranks = { 'profiles': [], 'fotoclubs': [] }
+    let ranks = { 'profiles': [], 'fotoclubs': r.fotoclubs }
     let cats  = {}
     let sects = {}
 
@@ -35,6 +43,7 @@ export class RankingPage implements OnInit {
         nombre_categoria: r.Category[c].name,
         id_categoria: r.Category[c].id,
         profiles: [],
+        pestania_seccion: -1,
         ranks_seccion: []
       })
     }
@@ -55,6 +64,7 @@ export class RankingPage implements OnInit {
     //se asignan los perfiles a los rankings
     for (let c=0; c < r.profiles.length; c++){
       let perfil = r.profiles[c]
+      perfil.premios_temporada = JSON.parse( perfil.premios_temporada )
       perfil['posicion_temporada'] = 0
       if (perfil.puntaje_temporada > 0){
         //se asigna un perfil al ranking de la categoria que el corresponde
@@ -116,6 +126,7 @@ export class RankingPage implements OnInit {
       let pos_categoria = 1;
       for(let i=0; i < ranks.profiles[c].profiles.length; i++){
         ranks.profiles[c].profiles[i].posicion_temporada = pos_categoria
+        ranks.profiles[c].profiles[i].premios_temporada = this.arreglo_premios( ranks.profiles[c].profiles[i].premios_temporada )
         if ((i+1 < ranks.profiles[c].profiles.length) && (ranks.profiles[c].profiles[i].puntaje_temporada != ranks.profiles[c].profiles[i+1].puntaje_temporada)){
           pos_categoria += 1
         }
@@ -123,38 +134,72 @@ export class RankingPage implements OnInit {
 
       let pos_seccion = 1;
       for (let i=0; i < ranks.profiles[c].ranks_seccion.length; i++){
-        console.log(ranks.profiles[c].ranks_seccion[i])
-
+        for(let j=0; j< ranks.profiles[c].ranks_seccion[i].profiles.length; j++ ){
+          ranks.profiles[c].ranks_seccion[i].profiles[j].posicion_temporada = pos_seccion
+          ranks.profiles[c].ranks_seccion[i].profiles[j].premios_temporada = this.arreglo_premios( ranks.profiles[c].ranks_seccion[i].profiles[j].premios_temporada )
+          if ((j+1 < ranks.profiles[c].ranks_seccion[i].profiles.length) && (ranks.profiles[c].ranks_seccion[i].profiles[j].puntaje_temporada != ranks.profiles[c].ranks_seccion[i].profiles[j+1].puntaje_temporada)){
+            pos_seccion += 1
+          }
+        }
       }
     }
 
-    console.log(ranks)
-/**
- * 
- * this.ranking.profiles = this.ranking.profiles.sort(
-        (item1, item2) => {
-          if (item1.score < item2.score ) {
-            return -1 * this.sentido_orden;
-          }
-          if (item1.score > item2.score) {
-            return 1 * this.sentido_orden;
-          }
-          return 0;
-        })
+    //Se ordenan los fotoclubes
+    ranks.fotoclubs.sort( (item1, item2) => {
+      if (item1.puntaje_temporada < item2.puntaje_temporada ) {
+        return -1 * this.sentido_orden;
+      }
+      if (item1.puntaje_temporada > item2.puntaje_temporada) {
+        return 1 * this.sentido_orden;
+      }
+      return 0;
+    })
 
-      this.ranking.fotoclubs = this.ranking.fotoclubs.sort(
-        (item1, item2) => {
-          if (item1.score < item2.score ) {
-            return -1 * this.sentido_orden;
-          }
-          if (item1.score > item2.score) {
-            return 1 * this.sentido_orden;
-          }
-          return 0;
-        }) 
- */
+    //se asignan los numeros de posicion
+    let pos_temporada = 1
+    for (let c=0; c < ranks.fotoclubs.length; c++){
+      ranks.fotoclubs[c]['posicion_temporada'] = pos_temporada
+      ranks.fotoclubs[c].porc_efectividad_anual = JSON.parse(ranks.fotoclubs[c].porc_efectividad_anual)
+      ranks.fotoclubs[c].premios_temporada = this.arreglo_premios(JSON.parse(ranks.fotoclubs[c].premios_temporada))
+
+      if (c+1 < ranks.fotoclubs.length && ranks.fotoclubs[c+1].puntaje_temporada != ranks.fotoclubs[c].puntaje_temporada){
+        pos_temporada += 1
+      }
+    }
 
     return ranks;
+  }
+
+  sumar_premios( obj1, obj2 ){
+    if (typeof obj1 == 'string'){
+      obj1 = JSON.parse( obj1 )
+    }
+    let salida = {...obj1}
+    
+    console.log(salida)
+    return obj1;
+  }
+
+  arreglo_premios( obj ){
+    let arr = []
+    
+    let ks = Object.keys(obj)
+    for (let c=0; c < ks.length; c++){
+      if (ks[c] != '0' && ks[c] != 'RECHAZADA')
+        arr.push({ 'nombre': ks[c], 'cantidad': obj[ks[c]] })
+    }
+
+    arr.sort( (item1, item2) => {
+      if (item1.cantidad < item2.cantidad ) {
+        return -1 * this.sentido_orden;
+      }
+      if (item1.cantidad > item2.cantidad) {
+        return 1 * this.sentido_orden;
+      }
+      return 0;
+    })
+
+    return arr;
   }
  
   cargarRanking() {
