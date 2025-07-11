97106ae63bffbd75361aea7d5743e01e1f22fe81
luciano <lucho.2012.tandil@gmail.com>
2025-07-11 16:37:31 -0300

    feat: agregar configuracion public_api

--- angular/src/app/services/config/config.service.ts
+++ angular/src/app/services/config/config.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 export const CONFIG = {
   apiBaseUrl: "https://gfc.prod-api.greenborn.com.ar/",//this.local ? "http://localhost:8888/" : "https://gfc.api.greenborn.com.ar/",
+  publicApi: "https://gfc.api2.greenborn.com.ar/",
   loginAction:"login",
   appName: "app_gfc_prod-",
   version: "1.1.32"
@@ -30,6 +31,10 @@ export class ConfigService {
 
   apiUrl(recurso: string) { 
     return this.data.apiBaseUrl + recurso
+  }
+
+  publicApiUrl(recurso: string) { 
+    return this.data.publicApi + recurso
   }
 
   setLocalStorage(r: string, v: any): void {
