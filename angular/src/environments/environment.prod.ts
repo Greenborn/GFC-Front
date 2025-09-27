2da6584a82e777e9be1345be4206f7fb079b1661
lucho.2012.tandil@gmail.com <lucho.2012.tandil@gmail.com>
2025-09-26 23:02:37 -0300

    se modifica posicion de boton de inscripcion

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,12 +3,12 @@
 
 export const environment = {
   production: true,
-  version: '1.8.12',
+  version: '1.8.11',
   // Configuración de la API
   apiBaseUrl: 'https://gfc.prod-api.greenborn.com.ar/',
   publicApi: 'https://gfc.api2.greenborn.com.ar/api/',
   loginAction: 'login',
-  appName: 'app_gfc_dev-',
+  appName: 'app_gfc_prod-',
   imagesBaseUrl: 'https://assets.prod-gfc.greenborn.com.ar',
   nodeApiBaseUrl: 'https://gfc.api2.greenborn.com.ar/api/'
 };
