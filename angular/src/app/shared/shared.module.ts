2c06d973e353011c090d335b07530fbbe6001061
luciano <lucho.2012.tandil@gmail.com>
2025-10-12 20:48:36 -0300

    Fix: Resolver errores de compilación de Angular
    - Agregar import de CondicionesServicioComponent en app.module.ts
    - Agregar IonicModule, FormsModule, ReactiveFormsModule y RouterModule a SharedModule
    - Eliminar atributos obsoletos push-sm y pull-sm de footer-post.component.html (incompatibles con Ionic 5+)
    - Corregir tipo de ViewChild en search-bar.component para usar IonSearchbar correctamente
    - Reorganizar orden de imports en app.module.ts (SharedModule antes de AppRoutingModule)
    
    Los cambios resuelven todos los errores de compilación en producción mientras mantienen el funcionamiento correcto en desarrollo.

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -1,5 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { UsuarioImgComponent } from './usuario-img/usuario-img.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { MenuAccionesComponent } from './menu-acciones/menu-acciones.component';
@@ -25,8 +28,17 @@ import { SearchSelectComponent } from './search-select/search-select.component';
   ],
   imports: [
     CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
   ],
   exports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
     UsuarioImgComponent, 
     SearchBarComponent, 
     SearchSelectComponent,
