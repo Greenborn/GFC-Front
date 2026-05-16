0b2468243021f3cd1dab8a7c17ca281fc23b730a
lucho.2012.tandil@gmail.com <lucho.2012.tandil@gmail.com>
2026-05-16 00:45:41 -0300

    [#464] En vista de ranking se ocultan botones de subsecciones sin ppuntuaciones; se usa nuevo endpoint nodeJS

--- angular/src/app/services/ranking.service.ts
+++ angular/src/app/services/ranking.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { ConfigService } from './config/config.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiSerializedResponse } from '../models/ApiResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,28 @@ export class RankingService extends ApiService<any> {
     super('ranking', http, config)
     this.fetchAllOnce = true
    }
+
+  getAll<K = any>(getParams: string = '', resource: string = null): Observable<K[]> {
+    if (this.fetchAllOnce && this.all != undefined) {
+      return new Observable(subscriber => {
+        subscriber.next(this.all as K[])
+      })
+    }
+
+    const url = this.config.nodeApiBaseUrl + `${resource ?? this.recurso}?${getParams}`
+    return this.http.get<ApiSerializedResponse<K>>(url).pipe(
+      map((data) => {
+        if (this.fetchAllOnce) {
+          this.all = data.items
+        }
+        if (data != null) {
+          this.all_meta = data._meta
+          return data.items
+        }
+        return null
+      })
+    )
+  }
 
    get template(): any {
     return {
