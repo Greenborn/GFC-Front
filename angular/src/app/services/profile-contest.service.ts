052b9b073abbcd76df46d1ddf82513cc5aeae169
luciano <lucho.2012.tandil@gmail.com>
2026-05-19 07:10:36 -0300

    se usa api nodejs para hacer get profile-registrable

--- angular/src/app/services/profile-contest.service.ts
+++ angular/src/app/services/profile-contest.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ProfileContest } from '../models/profile_contest';
 import { ApiService } from './api.service';
 import { ConfigService } from './config/config.service';
@@ -15,6 +16,14 @@ export class ProfileContestService extends ApiService<ProfileContest> {
   ) {
     super('profile-contest', http, config)
    }
+
+  getAll<K = ProfileContest>(getParams: string = '', resource: string = null): Observable<K[]> {
+    const endpoint = resource === 'profile-registrable'
+      ? `${this.config.nodeApiBaseUrl}${resource}?${getParams}`
+      : `${this.config.apiUrl(`${resource ?? this.recurso}?${getParams}`)}`;
+
+    return this.http.get<K[]>(endpoint);
+  }
 
    get template(): ProfileContest {
     return {
