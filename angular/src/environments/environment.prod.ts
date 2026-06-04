1bb3e800a7b2ac8422667f807fc9d6a7229f13a7
luciano <lucho.2012.tandil@gmail.com>
2026-06-04 16:37:58 -0300

    Se quita footer de todos lados ya que no tiene razon de ser y solo ocupa espacio; se mejora visualizacion de vista de detalle de ranking

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
   production: true,
-  version: '1.15.33',
+  version: '1.15.30',
   // Configuración de la API
   apiBaseUrl: 'https://gfc.prod-api.greenborn.com.ar/',
   publicApi: 'https://gfc.api2.greenborn.com.ar/api/',
