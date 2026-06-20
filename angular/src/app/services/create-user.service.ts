21ee6e938d12e4632410393a9c9ff3a9245dfe97
luciano <lucho.2012.tandil@gmail.com>
2026-06-19 21:11:06 -0300

    [#480] refactor(registro): integración SSO, cambio de endpoint y campos obligatorios

--- angular/src/app/services/create-user.service.ts
+++ angular/src/app/services/create-user.service.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { ApiChangePasswordBody } from '../models/ApiRequest';
 import { User } from '../models/user.model';
 import { ApiService } from './api.service';
 import { ConfigService } from './config/config.service';
@@ -16,13 +14,19 @@ export class CreateUserService extends ApiService<User> {
     http: HttpClient,
     config: ConfigService
   ) {
-    super('sign-up', http, config)
+    super('auth/register', http, config)
    }
 
    get template(): any {
-    return {
-     
-    }
-  }
+    return {}
+   }
+
+   post<K = User>(model: any, id: number = undefined, getParams: string = '', headers?: HttpHeaders): Observable<K> {
+    const h = headers ?? new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url = `${this.config.nodeApiBaseUrl}${this.recurso}`;
+    return id == undefined
+      ? this.http.post<K>(url, model, { headers: h })
+      : this.http.put<K>(url, model, { headers: h });
+   }
    
 }
