fda794f2be31cf599e44b36c44bdd5f0ac3abadc
luciano <lucho.2012.tandil@gmail.com>
2026-08-02 06:14:31 -0300

    [#503] feat(contest-judge): agregar método getAll para recuperar colección con metadatos
    - Incluye imports de axios/rxjs y nuevo getAll() en ContestJudgeService
    - Añade unique_id desde localStorage a los parámetros de consulta
    - Retorna Observable de items y guarda metadatos de paginación en all_meta

--- angular/src/app/services/contest-judge.service.ts
+++ angular/src/app/services/contest-judge.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { Observable, from } from 'rxjs';
 import { ContestJudge } from '../models/contest_judge.model';
 import { ApiService } from './api.service';
 import { ConfigService } from './config/config.service';
@@ -20,5 +22,22 @@ export class ContestJudgeService extends ApiService<ContestJudge> {
       contest_id: undefined,
       user_id: undefined,
     }
+  }
+
+  getAll<K = T>(getParams = '', resource: string | null = null): Observable<K[]> {
+    let params = getParams;
+    const uniqueId = localStorage.getItem('sso_client_unique_id');
+    if (uniqueId) {
+      const separator = params.includes('?') ? '&' : '?';
+      params += separator + 'unique_id=' + encodeURIComponent(uniqueId);
+    }
+    const path = this.getPath(resource);
+    const url = `${this.getBaseUrl()}${path}?${params}`;
+    return from(axios.get(url, { headers: this.getHeaders() }).then(r => {
+      const data = r.data as any;
+      const items = data?.items ?? data;
+      if (data?._meta != null) this.all_meta = data._meta;
+      return items;
+    }));
   }
 }
